a64edf4a534848c94b74e36cc92c7f083299b564
LucianoGodoy <luci.godoy@duocuc.cl>
2025-11-15 14:53:13 -0300

    Refactor config.js and index.html for clarity and consistency

--- admin/config.js
+++ admin/config.js
@@ -1,6 +1,6 @@
 window.CMS_CONFIG = {
   backend: {
-    name: "test-repo", // CAMBIAMOS EL BACKEND A MODO DE PRUEBA
+    name: "test-repo",
   },
   collections: [
     {
@@ -9,7 +9,6 @@ window.CMS_CONFIG = {
       folder: "ocs",
       create: true,
       fields: [
-        // Conservamos solo los campos esenciales
         { label: "Titulo (Nombre del OC)", name: "titulo", widget: "string" },
         { label: "Imagen Principal", name: "image", widget: "image" },
         { label: "Layout", name: "layout", widget: "hidden", default: "oc-layout.njk" },
